9fabe1831f60d14dac3afe63ac87ac677ba7839c
ranjiithravi <153696623+ranjiithravi@users.noreply.github.com>
2025-10-10 12:51:55 +1000

    Update md2pdf.py

--- md2pdf.py
+++ md2pdf.py
@@ -5,12 +5,16 @@ from markdown_pdf import MarkdownPdf, Section
 # Create a MarkdownPdf object
 pdf = MarkdownPdf()
 
+filePath = r'C:\Users\U8019277\OneDrive - UniSQ\BlastOne_project'+r'\\'
+fileName = r'safe_operation'
 # Load Markdown content from a file or string
-with open("input.md", "r") as f:
+with open(filePath+fileName+r'.md', "r") as f:
   markdown_content = f.read()
 
 # Add a section to the PDF
 pdf.add_section(Section(markdown_content))
 
 # Save the PDF
-pdf.save("output.pdf")
+pdf.save(filePath+fileName+r'.pdf')
+
+print('+++ PDF conversion completed +++')
